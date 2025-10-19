e03a367439533451491d818d88bc96bedd358ca1
kolkvicija <vaka.vice@gmail.com>
2025-10-20 01:11:08 +0200

    initial commit

---
+++ frontend/js/custom.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+
+  $("main#spapp > section").height($(document).height() - 60);
+
+  var app = $.spapp({pageNotFound : 'error_404'}); // initialize
+
+  // define routes
+  /*app.route({
+    view: 'view_1',
+    onCreate: function() { $("#view_1").append($.now()+': Written on create<br/>'); },
+    onReady: function() { $("#view_1").append($.now()+': Written when ready<br/>'); }
+  });?*/
+  app.route({
+    view: 'home', load: 'home.html' });
+  app.route({
+    view: 'shop', load: 'shop.html' });
+  
+
+  // run app
+  app.run();
+
+});
